8091ae6108ba88b13f49e45a93850e2fb6f8a84b
Alexander <drjmengelele@gmail.com>
2019-07-15 05:44:52 +0300

    new function

---
+++ DasBot/App_Code/Distance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DasBot.App_Code
+{
+    public static class Distance
+    {
+        public static double ModifyedLevensteinDistance(string a, string b) => 2 * Accord.Math.Distance.Levenshtein(a, b) / (a.Length + b.Length);
+    }
+}
--- DasBot/Controllers/CallbackController.cs
+++ DasBot/Controllers/CallbackController.cs
@@ -36,12 +36,29 @@ namespace DasBot.Controllers
                 case "message_new":
                     {
                         var msg = Message.FromJson(new VkResponse(updates.Object));
-
+                        string result;
+                        var words = msg.Text.Split(' ').Where(word => word.Length > 0).ToArray();
+                        if (words.Length < 2)
+                            result = "Мне кажется, тут не 2 слова...";
+                        else if (words.Length > 2)
+                            result = "Мне кажется, что тут больше двух слов...";
+                        else
+                        {
+                            var d = App_Code.Distance.ModifyedLevensteinDistance(words[0], words[1]);
+                            if (d == 0)
+                                result = "Эти два слова одинаковы!";
+                            else if (d <= 0.25)
+                                result = "Эти два слова очень похожи.";
+                            else if (d <= 0.40)
+                                result = "Эти два слова чем-то похожи.";
+                            else
+                                result = "Эти два слова не похожи";
+                        }
                         _vkApi.Messages.Send(new MessagesSendParams
                         {
                             RandomId = new DateTime().Millisecond,
                             PeerId = msg.PeerId.Value,
-                            Message = msg.Text + " ы"
+                            Message = result
                         });
                         break;
                     }
